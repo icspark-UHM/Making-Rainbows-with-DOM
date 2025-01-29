8eeb350b147baf1b65b641276fd588d146b77416
Lukas Moroz <lukasachu@gmail.com>
2025-01-28 18:59:52 -1000

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Task 1: Change the cloud to stop the rain
+const cloud = document.getElementById('cloud');
+cloud.src = 'images/cloud1.png';
+
+// Task 2: Make the rainbow visible
+const rainbow = document.getElementById('rainbow');
+rainbow.style.display = 'flex';
+
